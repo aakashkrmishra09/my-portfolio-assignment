05644809a82b51276747d311ac350609948d6610
aakashkrmishra09 <aakashkrmishra09@gmail.com>
2025-11-29 18:56:26 -0500

    Update API URL for production

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
+//const API_URL = 'http://localhost:8080/api';
+const API_URL = 'https://aakashkumarmishra-backend.onrender.com//api';
 
 const api = axios.create({
   baseURL: API_URL,
